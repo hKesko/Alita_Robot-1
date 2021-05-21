689486c2a8a7abfd5ff4c48c203e49a6820fd9a2
hKesko <81475460+hKesko@users.noreply.github.com>
2021-05-22 00:49:05 +0800

    Update __init__.py

--- alita/__init__.py
+++ alita/__init__.py
@@ -72,7 +72,7 @@ LOGGER.info("------------------------")
 LOGGER.info("|      Alita_Robot     |")
 LOGGER.info("------------------------")
 # LOGGER.info(f"Version: {Config.VERSION}")
-LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
+# LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
 LOGGER.info("Source Code: https://github.com/Divkix/Alita_Robot\n")
 
 # Account Related
